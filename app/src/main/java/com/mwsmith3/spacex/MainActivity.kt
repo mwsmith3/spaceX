cba022fe3f06bd3aa1f78a1e6b3d5d30c17a2c73
Matthew Smith <mwsmith3@googlemail.com>
2021-06-16 20:38:30 +0100

    initialise

---
+++ app/src/main/java/com/mwsmith3/spacex/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mwsmith3.spacex
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
