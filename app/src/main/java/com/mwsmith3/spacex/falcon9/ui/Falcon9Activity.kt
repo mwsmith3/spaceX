b2953c00c2f47e0afd412a458799d02b62e8fe29
Matthew Smith <mwsmith3@googlemail.com>
2021-06-16 20:45:54 +0100

    Created package structure, corrected naming

--- app/src/main/java/com/mwsmith3/spacex/falcon9/ui/Falcon9Activity.kt
+++ app/src/main/java/com/mwsmith3/spacex/falcon9/ui/Falcon9Activity.kt
@@ -1,11 +1,12 @@
-package com.mwsmith3.spacex
+package com.mwsmith3.spacex.falcon9.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mwsmith3.spacex.R
 
-class MainActivity : AppCompatActivity() {
+class Falcon9Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_falcon9)
     }
 }
